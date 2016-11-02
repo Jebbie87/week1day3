7d6480b45e33958dbecdedaa1379af072e76f4b7
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-02 16:03:45 +0000

    Finish letter-count

---
+++ letter-count/letter-count.js
@@ -0,0 +1,17 @@
+// lighthouse in the house
+var word = "lighthouse in the house";
+function lettersLetters (str) {
+  var noSpaces = str.split(" ").join("").toLowerCase();
+  var letters = {};
+
+   for (var i = 0; i < noSpaces.length; i++) {
+      if (letters.hasOwnProperty(noSpaces[i])) {
+        letters[noSpaces[i]]++;
+      } else {
+        letters[noSpaces[i]] = 1
+      }
+  }
+  return letters;
+}
+
+console.log(lettersLetters(word));
