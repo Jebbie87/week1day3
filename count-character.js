7486bcc2b675e40f06b3b85227bf0db7ab27324c
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-11-02 17:40:36 +0000

    Forgot to save program

--- count-character.js
+++ count-character.js
@@ -3,13 +3,26 @@ function lettersLetters (str) {
   var noSpaces = str.split(" ").join("").toLowerCase();
   var letters = {};
 
-   for (var i = 0; i < noSpaces.length; i++) {
+   /*for (var i = 0; i < noSpaces.length; i++) {
       if (letters.hasOwnProperty(noSpaces[i])) {
         letters[noSpaces[i]]++;
       } else {
         letters[noSpaces[i]] = 1
       }
+  }*/
+
+  for ()
+
+  for (var i = 0; i < noSpaces.length; i++){
+    if (letters.hasOwnProperty(noSpaces[i])) {
+        letters[noSpaces[i]].push(i);
+      } else {
+        letters[noSpaces[i]] = [i]
+      }
   }
+
+  letters.
+
   return letters;
 }
 
